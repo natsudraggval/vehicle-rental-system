35da5395f4de7d46518f7b981a6b6bdb47903f65
natusdraggval <natsudraggval@gmail.com>
2025-11-08 22:59:43 +0545

    merged profile in home userdash

--- Frontend/src/Pages/UserDashboard/Home.jsx
+++ Frontend/src/Pages/UserDashboard/Home.jsx
@@ -26,6 +26,33 @@ function Home() {
                 </div>
             </div>
 
+            <div
+                class="bg-white shadow-md p-6 rounded-lg hover:shadow-md transition"
+            >
+                <h3 class="text-2xl font-bold text-gray-800 mb-6">My Profile</h3>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div>
+                        <p class="text-gray-500 font-medium mb-1">Full Name</p>
+                        <p class="text-gray-800">John Doe</p>
+                    </div>
+
+                    <div>
+                        <p class="text-gray-500 font-medium mb-1">Email Address</p>
+                        <p class="text-gray-800">johndoe@email.com</p>
+                    </div>
+
+                    <div>
+                        <p class="text-gray-500 font-medium mb-1">Phone Number</p>
+                        <p class="text-gray-800">+977-9800000000</p>
+                    </div>
+
+                    <div>
+                        <p class="text-gray-500 font-medium mb-1">Address</p>
+                        <p class="text-gray-800">Kathmandu, Nepal</p>
+                    </div>
+                </div>
+            </div>
+
             <footer
                 class="bg-white p-4 text-center text-sm text-gray-400 rounded-lg shadow-md border-t border-gray-200"
             >
