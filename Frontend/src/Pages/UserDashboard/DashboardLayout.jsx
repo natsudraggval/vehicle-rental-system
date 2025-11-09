f525b05c8c13f35190513070afe2a307ecac2c57
natusdraggval <natsudraggval@gmail.com>
2025-11-10 00:17:21 +0545

    okk

--- Frontend/src/Pages/UserDashboard/DashboardLayout.jsx
+++ Frontend/src/Pages/UserDashboard/DashboardLayout.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
-
 function DashboardLayout() {
   return (
-    <div className="bg-gray-100 font-sans flex">
-      <Sidebar />
-      <div className="flex-1 ml-64">
-        <main className="p-5 bg-gray-100">
-          <Outlet />  {/* render the page section or content here */}
-        </main>
+    <div className="min-h-screen bg-gray-100 font-sans"> {/* ensure full viewport height */}
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 ml-64 min-h-screen"> {/* make main stretch full height */}
+          <main className="p-5">
+            <Outlet />  {/* render the page section or content here */}
+          </main>
+        </div>
       </div>
     </div>
   );
